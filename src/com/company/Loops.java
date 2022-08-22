0a77370790b51777a9a172bd4c02703b7de2775c
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-08-22 18:17:50 +0600

    Reverse Number

--- src/com/company/Loops.java
+++ src/com/company/Loops.java
@@ -41,16 +41,44 @@
             //            System.out.println("UpperCase");
             //        }
 
-            //Fibonacchi nth number
-            Scanner sc = new Scanner(System.in);
-            int nth = sc.nextInt();
-            int prev = 0;
-            int next = 1;
-            for (int i = 0; i < nth; i++) {
-                int temp= next;
-                next = next+prev;
-                prev =temp;
-            }
-            System.out.println(prev);
+//            //Fibonacchi nth number
+//            Scanner sc = new Scanner(System.in);
+//            int nth = sc.nextInt();
+//            int prev = 0;
+//            int next = 1;
+//            for (int i = 0; i < nth; i++) {
+//                int temp= next;
+//                next = next+prev;
+//                prev =temp;
+//            }
+//            System.out.println(prev);
+
+
+            //Counting Occurance.
+//            int number= 2525252;
+//            int count=0;
+//            while(number>0){
+//                int remainder_number=number%10;
+//                if(remainder_number==2) {
+//                    count++;
+//                }
+//                number = number/10;
+//            }
+//            System.out.println("Total number of occurance is "+count);
+
+
+            //Reverse Number
+//            int number=12345679;
+//            int rev_number=0;
+//            while(number>0){
+//                int remainder_number=number%10;
+//                number=number/10;
+//                rev_number=rev_number*10+remainder_number;
+//            }
+//            System.out.println(rev_number);
+
+
+            //Calculator program
+
         }
     }
