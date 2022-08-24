e08af80f52f11612279fba341eb92d86b9220335
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-08-24 09:00:43 +0600

    efficient build

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -2,36 +2,35 @@ package com.company;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr= {2,4,5,7,10,12,14,30,32,40};
-        int targetElem= 40;
-        int start=0;
-        int end= arr.length;
-        int mid=end/2;
-        if(arr[mid]==targetElem){
-            System.out.println("you target element is in "+mid+" index");
+        int[] arr = {2, 4, 5, 7, 10, 12, 14, 30, 32, 40};
+        int targetElem = 5;
+        System.out.println("Your Element in index " + searchElement(arr, targetElem));
+    }
+
+    static int searchElement(int[] arr, int targetElement)
+    {
+        int start=0, end= arr.length, mid=end/2;
+        if(arr[mid]==targetElement){
+            return mid;
         }
-        else if(targetElem<arr[mid]){
+        else if(targetElement<arr[mid]){
             mid=end;
             while(start<=end){
-                if(arr[start]==targetElem){
-                    System.out.println("your target element is in "+ start);
-                    break;
+                if(arr[start]==targetElement){
+                    return start;
                 }
                 start++;
             }
         }
-        else if(targetElem>arr[mid]){
+        else if(targetElement>arr[mid]){
             start=mid+1;
             while(start<=end){
-                if(arr[start]==targetElem){
-                    System.out.println("your target element is in "+ start);
-                    break;
+                if(arr[start]==targetElement){
+                    return start;
                 }
                 start++;
             }
         }
-        else{
-            System.out.println("Wrong input");
-        }
+        return -1;
     }
 }
