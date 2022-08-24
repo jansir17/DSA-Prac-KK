0bfa05646761ea6cc6e8e65652c56971c81fff79
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-08-24 09:24:32 +0600

    Another way of Binary S

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -3,13 +3,35 @@ package com.company;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2, 4, 5, 7, 10, 12, 14, 30, 32, 40};
-        int targetElem = 5;
-        System.out.println("Your Element in index " + searchElement(arr, targetElem));
+        int targetElem1 = 5;
+        System.out.println("Your Element in index " + searchElement(arr, targetElem1));
+        int targetElem2=32;
+        System.out.println("Your Element in index "+searchElement2(arr,targetElem2));
+    }
+    //Efficient way
+    static int searchElement2(int[] arr, int targetElement){
+        int start =0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if(targetElement<arr[mid]){
+                end=mid-1;
+            }
+            else if(targetElement>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
     }
 
+    //Easy way
     static int searchElement(int[] arr, int targetElement)
     {
         int start=0, end= arr.length, mid=end/2;
+        //if we have a big integer number, we can take mid as, start+(end-start)/2, which is same.
         if(arr[mid]==targetElement){
             return mid;
         }
