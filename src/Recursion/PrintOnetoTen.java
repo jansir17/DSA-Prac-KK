66f5b18a890387e4ed2cb8e86f6eb2f51836b05e
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-09-25 08:52:24 +0600

    1to10

--- src/Recursion/PrintOnetoTen.java
+++ src/Recursion/PrintOnetoTen.java
@@ -2,6 +2,12 @@ package Recursion;
 
 public class PrintOnetoTen {
     public static void main(String[] args) {
-
+        printNumber(1);
+    }
+    static void printNumber(int n){
+        if(n<=100){
+            System.out.print(n+" ");
+            printNumber(n+1);
+        }
     }
 }
